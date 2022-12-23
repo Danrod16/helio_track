fffb80e2e1a33cc9a3f46e8bee50b298dc0ad9e2
Daniel Rodriguez <daniel.rod93@gmail.com>
2022-12-23 10:52:42 +0100

    Refacto: refactored tests into fixtures

--- test/models/company_test.rb
+++ test/models/company_test.rb
@@ -6,42 +6,34 @@ class CompanyTest < ActiveSupport::TestCase
   # end
 
   test "should not save company without name" do
-    company = Company.new
+    company = companies(:google)
     assert_not company.save, "Saved the company without a name"
   end
 
   test "should not save company without industry" do
-    company = Company.new
+    company = companies(:google)
     assert_not company.save, "Saved the company without an industry"
   end
 
   test "should not save company without website" do
-    company = Company.new
+    company = companies(:google)
     assert_not company.save, "Saved the company without a website" 
   end
 
   test "website url should be valid" do
-    company = Company.new
-    company.name = "Google"
-    company.industry = "Technology"
+    company = companies(:google)
     company.website = "http://www.google.com"
-    company.target = "Entrepreneurs"
-    assert company.save!, "Saved the company with a valid website"
+    assert company.save, "Saved the company with a valid website"
   end
 
   test "website url should not be invalid" do
-    company = Company.new
+    company = companies(:google)
     company.website = "http://www.google"
     assert_not company.save, "invalid website"
   end
 
   test "should not save company without target" do
-    company = Company.new
+    company = companies(:google)
     assert_not company.save, "Saved the company without a target"
   end
-
-  test "should have many users" do
-    company = Company.new
-    assert_respond_to company, :users
-  end 
 end
