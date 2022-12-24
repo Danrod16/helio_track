8ddacc4463227fd64497d3b64a1d35b680ac3913
Daniel Rodriguez <daniel.rod93@gmail.com>
2022-12-24 11:01:26 +0100

    Fixed validations order error

--- app/models/step.rb
+++ app/models/step.rb
@@ -2,8 +2,8 @@ class Step < ApplicationRecord
   belongs_to :journey
   validates :name, presence: true
   validates :status, presence: true, inclusion: { in: %w(active completed pending) }
-  has_many :templates, through: :step_templates
   has_many :step_templates, dependent: :destroy
+  has_many :templates, through: :step_templates
   has_many :tickets, dependent: :destroy
   has_many :users, through: :user_tasks
 end
--- app/models/template.rb
+++ app/models/template.rb
@@ -1,5 +1,5 @@
 class Template < ApplicationRecord
     validates :name, presence: true
-    has_many :steps, through: :step_templates
     has_many :step_templates, dependent: :destroy
+    has_many :steps, through: :step_templates
 end
